3e6761ebf2ad0a43e740b13c06addc93ccf387aa
0xAckerMan <joelkores1@gmail.com>
2022-11-07 17:59:50 +0300

    Randint func in random

---
+++ 30 Days/libraries/0x0-random/1_randint.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Author: @K0r3s
+
+'''
+prints random numbers from 1 to 20 in an equal probability
+'''
+
+import random
+
+number = random.randint(1,20)
+
+print ("The random number is: ",number)

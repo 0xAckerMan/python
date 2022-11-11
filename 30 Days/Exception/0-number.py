74271515533aebfe978526dc44c202c6ca51a498
0xAckerMan <joelkores1@gmail.com>
2022-11-11 12:27:56 +0300

    Handling ValueError

---
+++ 30 Days/Exception/0-number.py
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+'''
+Using try and except to cat user input errors
+when a user does not enters an int to the program
+'''
+
+try:
+    x = int(input("Enter x: "))
+    print(f"x is {x}")
+
+except ValueError:
+    print('x is not an integer')

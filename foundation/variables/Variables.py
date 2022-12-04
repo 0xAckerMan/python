49274e2da86e97f57222c68473d92f5c143efa57
0xAckerMan <joelkores1@gmail.com>
2022-12-04 11:06:29 +0300

    Operators

---
+++ foundation/variables/Variables.py
@@ -0,0 +1,96 @@
+# Author: k0r3s
+
+# Day 2: 30 Days of python programming
+# Exercises: Level 1
+
+# Declare a first name variable and assign a value to it
+first_name = 'John'
+
+# Declare a last name variable and assign a value to it
+last_name = 'Doe'
+
+# Declare a full name variable and assign a value to it
+full_name = 'John Doe'
+
+# Declare a country variable and assign a value to it
+country = "Germany"
+
+# Declare a city variable and assign a value to it
+city = "Denmark"
+
+# Declare an age variable and assign a value to it
+age = 45
+
+# Declare a year variable and assign a value to it
+year = 2030
+
+# Declare a variable is_married and assign a value to it
+is_married = True
+
+# Declare a variable is_true and assign a value to it
+is_true = False
+
+# Declare a variable is_light_on and assign a value to it
+is_light_on = False
+
+# Declare multiple variable on one line
+hobby, language = "coding", "Python"
+
+
+# Exercises: Level 2
+# Check the data type of all your variables using type() built-in function
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(age))
+print(type(is_married))
+
+# Using the len() built-in function, find the length of your first name
+print(len(first_name))
+
+#Compare the length of your first name and your last name
+print(first_name == last_name)
+
+# Declare 5 as num_one and 4 as num_two
+num_one = 5
+num_two = 4
+
+# i. Add num_one and num_two and assign the value to a variable total
+total = num_one + num_one
+
+# ii. Subtract num_two from num_one and assign the value to a variable diff
+variable_diff = num_two - num_one
+
+# iii. Multiply num_two and num_one and assign the value to a variable product
+product = num_two * num_one
+
+# iv. Divide num_one by num_two and assign the value to a variable division
+division = num_one / num_two
+
+# v. Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+remainder = num_two % num_one
+
+# vi. Calculate num_one to the power of num_two and assign the value to a variable exp
+exp = num_two ** num_one
+
+# vii. Find floor division of num_one by num_two and assign the value to a variable floor_division
+floor_division = num_one // num_two
+
+
+
+#The radius of a circle is 30 meters.
+# i. Calculate the area of a circle and assign the value to a variable name of area_of_circle
+PI = 3.142
+radius = int(input("Enter the radius of the circle: "))
+area_of_circle = PI*radius**2
+
+# ii. Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+circum_of_circle = 2*PI*radius
+
+# iii. Take radius as user input and calculate the area.
+
+print(area_of_circle)
+
+
+# Run help('keywords') in Python shell or in your file to check for the Python reserved words or keywords
+help("keywords")

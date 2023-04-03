401fa2af7fdfc2f2c5456f34543e2b89ab85595c
0xAckerMan <joelkores1@gmail.com>
2023-04-03 15:19:47 +0300

    Regular for loop

---
+++ foundation/comprehession/0-list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+names = ["Joel", "Max","Jimmy"]
+upper_names = []
+
+for name in names:
+    upper_case_name = name.upper()
+    upper_names.append(upper_case_name)
+
+print(upper_names)
+

fa8893a55339a1e2a806f4a438fa829613875397
0xAckerMan <joelkores1@gmail.com>
2023-04-05 09:33:02 +0300

    Chore: Creating a private isnstance attr

---
+++ foundation/OOP/refresh/0x6-py-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+'''
+Class square
+'''
+
+
+class Square:
+    '''
+    Has private instance size
+    '''
+    def __init__(self, size):
+        '''
+        args:
+            size: Size of square
+        Attribute:
+            size: Private instance attribute
+        '''
+        self.size = size
+

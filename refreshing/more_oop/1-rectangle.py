f91278c640cde1a4489667be59054b82318cea63
0xAckerMan <joelkores1@gmail.com>
2024-01-03 01:22:03 +0300

    more oop concepts

---
+++ refreshing/more_oop/1-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, width):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        elif width < 0 :
+            raise ValueError("width must be > 0")
+        self.__width = width
+        
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
